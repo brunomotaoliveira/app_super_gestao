a76863dc08983bd7b2c006e0ceb187c9211f75b3
brunomotadev <brunomotadev@gmail.com>
2021-12-07 16:21:27 -0300

    agrupando rotas

--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,13 @@ Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])
 Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobrenos']);
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
 
-Route::get('/login', function(){ return 'Login';});
-Route::get('clientes', function(){ return 'Clientes'; });
-Route::get('/fornecedores', function(){ return 'Fornecedores'; });
-Route::get('/produtos', function(){ return 'Produtos'; });
+
+Route::prefix('/app')->group(function(){
+    Route::get('/login', function(){ return 'Login';});
+    Route::get('/clientes', function(){ return 'Clientes'; });
+    Route::get('/fornecedores', function(){ return 'Fornecedores'; });
+    Route::get('/produtos', function(){ return 'Produtos'; });
+});
 
 
 //ROTA DE TESTE
