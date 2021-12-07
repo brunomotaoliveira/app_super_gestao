2211efa6f974ead6c6b6bc36d09005d7fe28d098
brunomotadev <brunomotadev@gmail.com>
2021-12-06 21:56:03 -0300

    criado as rotas sobre e contato

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Olá seja bem vindo ao curso';
 });
+
+Route::get('/sobre-nos', function() {
+    return 'Sobre nos';
+});
+
+Route::get('/contato', function() {
+    return 'Contato';
+});
+
+/**
+ * Principais verbos HTTP
+ * 
+ * get
+ * post
+ * put
+ * patch
+ * delete
+ * options
+ * 
+ */
