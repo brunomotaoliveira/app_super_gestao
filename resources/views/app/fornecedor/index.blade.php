31039c483b7e5b30ebc8ddc83e92b0273e92df78
brunomotadev <brunomotadev@gmail.com>
2021-12-08 16:55:28 -0300

    incluindo comentarios e blocos php puros

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,16 @@
+<h3>Fornecedor</h3>
+
+
+{{ 'Texto' }}
+<?= 'texto texto' ?>
+{{-- comentario que sera descartado pelo interpretaor do blade --}}
+
+@php
+    // comentarios de uma linha em PHP
+    /**
+        comentarios de multiplas linhas PHP
+    */
+
+
+    echo 'texto';
+@endphp
