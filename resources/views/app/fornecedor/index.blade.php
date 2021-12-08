2f223aca636688cab79bd6bbe40e2f1a68c9c3d9
brunomotadev <brunomotadev@gmail.com>
2021-12-08 18:41:52 -0300

    blade unless

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -3,20 +3,23 @@
 
 @php
 
-    /* if() {
-
-    } elseif() {
-
-    } else {
-
-    } */
+    /* 
+    if(!<condicao>) {} // executa enquanto o retorno for true
+     */
 
 @endphp
 
-@if (count($fornecedores) > 0 && count($fornecedores) < 18)
-    <h3>Existem alguns. fornecedores cadastrados</h3>
-@elseif(count($fornecedores) > 10)
-    <h3>Existem vários fornecedores cadastrados</h3>
-@else
-    <h3>Ainda não existem fornecedores cadastrados</h3>
+{{-- @unless executa se o retorno for false --}}
+
+Fornecedor: {{ $fornecedores[0]['nome'] }}
+<br>
+Status: {{ $fornecedores[0]['status'] }}
+<br>
+@if( !($fornecedores[0]['status'] == 'S'))
+    Fornecedor inativo
 @endif
+<br>
+@unless($fornecedores[0]['status'] == 'S') <!-- executa se o retorno da condição for falso -->
+    fornecedor inativo
+@endunless
+<br>
