2c3d90950e25e859105a8f1ad939cd9f0909b9c5
brunomotadev <brunomotadev@gmail.com>
2021-12-08 18:59:09 -0300

    blade isset

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -4,22 +4,18 @@
 @php
 
     /* 
-    if(!<condicao>) {} // executa enquanto o retorno for true
+    if(isset($variavel)) {} // retorna true se a variavel estiver definida
      */
 
 @endphp
 
-{{-- @unless executa se o retorno for false --}}
+@isset($fornecedores)
+    Fornecedor: {{ $fornecedores[0]['nome'] }}
+    <br>
+    Status: {{ $fornecedores[0]['status'] }}
+    <br>
+    @isset($fornecedores[0]['cnpj'])
+        CNPJ: {{ $fornecedores[0]['cnpj'] }}
+    @endisset
+@endisset
 
-Fornecedor: {{ $fornecedores[0]['nome'] }}
-<br>
-Status: {{ $fornecedores[0]['status'] }}
-<br>
-@if( !($fornecedores[0]['status'] == 'S'))
-    Fornecedor inativo
-@endif
-<br>
-@unless($fornecedores[0]['status'] == 'S') <!-- executa se o retorno da condição for falso -->
-    fornecedor inativo
-@endunless
-<br>
