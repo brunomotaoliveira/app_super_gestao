2c0af60425ad149dbd0b6e523daf056fa59644c5
brunomotadev <brunomotadev@gmail.com>
2021-12-10 17:06:59 -0300

    blade laco while

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -21,7 +21,8 @@
 
 @isset($fornecedores)
 
-    @for($i = 0; isset($fornecedores[$i]); $i++)
+    @php $i = 0 @endphp
+    @while(isset($fornecedores[$i]))
         Fornecedor: {{ $fornecedores[$i]['nome'] }}
         <br>
         Status: {{ $fornecedores[$i]['status'] }}
@@ -30,7 +31,8 @@
         <br>
         Telefone: ({{ $fornecedores[$i]['ddd'] ?? '' }}) {{ $fornecedores[$i]['telefone'] ?? '' }}
         <hr>
-    @endfor
+        @php $i++ @endphp
+    @endwhile
 
 @endisset
 <br>
