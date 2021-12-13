76fbe82b89c9ce4b078c4fc96ba12343dbda03e2
brunomotadev <brunomotadev@gmail.com>
2021-12-12 23:07:58 -0300

    blade variavel loop

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -22,6 +22,8 @@
 @isset($fornecedores)
 
     @forelse($fornecedores as $indice => $fornecedor)
+        Interação atual: {{ $loop->iteration }}
+        <br>
         Fornecedor: {{ $fornecedor['nome'] }}
         <br>
         Status: {{ $fornecedor['status'] }}
@@ -29,6 +31,18 @@
         CNPJ: {{ $fornecedor['cnpj'] ?? 'Dado não foi preenchido' }}
         <br>
         Telefone: ({{ $fornecedor['ddd'] ?? '' }}) {{ $fornecedor['telefone'] ?? '' }}
+        <br>
+        @if($loop->first)
+            Primeira interação do loop
+        @endif
+
+        @if($loop->last)
+            Ultima interação do loop
+
+            <br>
+
+            Total de registros: {{ $loop->count }}
+        @endif
         <hr>
     @empty
         Não existem fornecedores cadastrados
